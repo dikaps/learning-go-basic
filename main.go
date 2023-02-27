9c5ddfa55a55f77cd5e2fde33edfc232b5e56b9c
andikaPs <andikapermanasidiq00@gmail.com>
2023-02-27 21:23:50 +0700

    learning go data type

--- main.go
+++ main.go
@@ -2,35 +2,42 @@ package main
 
 import "fmt"
 
+func batas() {
+	fmt.Println("-----------------------------------")
+}
+
 func main() {
-	var firstName string = "Andika Permana"
 
-	var lastName, _ string
-	lastName = "Sidiq"
+	// tipe data numerik non ddesimal
+	var positiveNumber uint8 = 82
+	var negativeNumber = -1243423644
 
-	fmt.Printf("Halo %s %s!\n", firstName, lastName)
+	fmt.Printf("Bilangan positif %d\n", positiveNumber)
+	fmt.Printf("Bilangan negatif %d\n", negativeNumber)
 
-	/*
-		deklrasi variabel tanpa tipe data ( type inference )
-		type inference merupakan sebuah teknik pendeklrasian variable
-		 tanpa mendefinisikan keyword var berikut dengan tipe datanya
-	*/
-	tanpaTipeData := "ini tuh string"
-	fmt.Printf("deklrasi variabel tanpa keyword var %s\n", tanpaTipeData)
+	batas()
 
-	// deklarasi multiple variabel
-	one, two := 1, "abcd"
+	// tipe data numerik desimal
+	var decimalNumber = 3.85
+	fmt.Printf("bilangan desimal %f \n", decimalNumber)
+	fmt.Printf("bilangan desimal %.3f \n", decimalNumber)
 
-	fmt.Println("deklrasi multiple variabel", one, two)
+	batas()
 
-	// Variabel Underscore digunakan untuk menampung nilai yang tidak akan dipakai
+	// tipe data boolean
+	var isExist bool = true
+	fmt.Printf("Is Exist? %t \n", isExist)
 
-	_ = "WOw"
-	_ = "eh"
+	batas()
 
-	// deklarasi variabel dengan keyword new
-	name := new(string)
-	fmt.Println(name)
-	fmt.Println(*name)
+	// tipe data string
 
+	var fullName string = "Andika Permana Sidiq"
+
+	fmt.Printf("Nama saya adalah %s \n", fullName)
+
+	var message = `Halo saya sedang mendeklarasikan variabel menggunakan backticks "hahaahha"
+kalo di enter dia akan di print enter juga "hehe"
+	`
+	fmt.Println(message)
 }
