f5a9936aed3174294b04d010a4042ffa2060d2ae
andikaPs <andikapermanasidiq00@gmail.com>
2023-02-27 21:55:57 +0700

    learning go about operator aritmatika, perbandingan, logika

--- main.go
+++ main.go
@@ -2,28 +2,32 @@ package main
 
 import "fmt"
 
-func batas() {
-	fmt.Println("-----------------------------------")
+func judul(title string) {
+	const line string = "-----"
+	fmt.Println(line, title, line)
 }
 
-// Konstanta
+// Operator
 
 func main() {
-	const firstName = "dIkaps"
+	judul("Operator Aritmatika")
+	var value = (((2+6)%3)*4 - 2) / 3
+	fmt.Println(value)
 
-	fmt.Print("halo ", firstName, "\n")
+	judul("Operator Perbandingan")
+	var isEqual bool = value == 2
+	fmt.Printf("nilai %d == %t \n", value, isEqual)
 
-	batas()
+	judul("Operator Logika")
+	const left = false
+	const right = true
 
-	// desklarasi multi konstanta
-	const (
-		square         = "kotak"
-		isToday  bool  = false
-		numeric  uint8 = 1
-		floatNum       = 2.2
-		pertama        = "ini string"
-		kedua          // ketika pendeklarasian konstanta tanpa nilai, maka nilai nya akan berisi nilai dari variabel yang diatasnya
-	)
+	const leftAndRight = left && right
+	fmt.Printf("left && right \t = %t \n", leftAndRight)
 
-	fmt.Println(square, isToday, numeric, floatNum, pertama, kedua)
+	const leftOrRight = left || right
+	fmt.Printf("left || right \t = %t \n", leftOrRight)
+
+	const leftReverse = !left
+	fmt.Printf("!left \t = %t \n", leftReverse)
 }
