798d3fdb23ef1ebb7f87eee3ae36d577db88dcdd
andikaPs <andikapermanasidiq00@gmail.com>
2023-02-27 21:40:56 +0700

    learning go about constant

--- main.go
+++ main.go
@@ -6,38 +6,24 @@ func batas() {
 	fmt.Println("-----------------------------------")
 }
 
+// Konstanta
+
 func main() {
+	const firstName = "dIkaps"
 
-	// tipe data numerik non ddesimal
-	var positiveNumber uint8 = 82
-	var negativeNumber = -1243423644
-
-	fmt.Printf("Bilangan positif %d\n", positiveNumber)
-	fmt.Printf("Bilangan negatif %d\n", negativeNumber)
+	fmt.Print("halo ", firstName, "\n")
 
 	batas()
 
-	// tipe data numerik desimal
-	var decimalNumber = 3.85
-	fmt.Printf("bilangan desimal %f \n", decimalNumber)
-	fmt.Printf("bilangan desimal %.3f \n", decimalNumber)
+	// desklarasi multi konstanta
+	const (
+		square         = "kotak"
+		isToday  bool  = false
+		numeric  uint8 = 1
+		floatNum       = 2.2
+		pertama        = "ini string"
+		kedua          // ketika pendeklarasian konstanta tanpa nilai, maka nilai nya akan berisi nilai dari variabel yang diatasnya
+	)
 
-	batas()
-
-	// tipe data boolean
-	var isExist bool = true
-	fmt.Printf("Is Exist? %t \n", isExist)
-
-	batas()
-
-	// tipe data string
-
-	var fullName string = "Andika Permana Sidiq"
-
-	fmt.Printf("Nama saya adalah %s \n", fullName)
-
-	var message = `Halo saya sedang mendeklarasikan variabel menggunakan backticks "hahaahha"
-kalo di enter dia akan di print enter juga "hehe"
-	`
-	fmt.Println(message)
+	fmt.Println(square, isToday, numeric, floatNum, pertama, kedua)
 }
