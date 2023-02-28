73deac569152e52b14933c4eca7181ca45567de7
andikaPs <andikapermanasidiq00@gmail.com>
2023-02-28 20:24:49 +0700

    learning go about seleksi kondisi

--- main.go
+++ main.go
@@ -10,24 +10,85 @@ func judul(title string) {
 // Operator
 
 func main() {
-	judul("Operator Aritmatika")
-	var value = (((2+6)%3)*4 - 2) / 3
-	fmt.Println(value)
+	judul("Seleksi Kondisi")
 
-	judul("Operator Perbandingan")
-	var isEqual bool = value == 2
-	fmt.Printf("nilai %d == %t \n", value, isEqual)
+	var point = 8
 
-	judul("Operator Logika")
-	const left = false
-	const right = true
+	if point == 10 {
+		fmt.Println("Lulus dengan nilai sempurna")
+	} else if point >= 5 {
+		fmt.Println("lulus")
+	} else if point == 4 {
+		fmt.Println("hampir lulus")
+	} else {
+		fmt.Printf("tidak lulus. nilai anda %d \n", point)
+	}
 
-	const leftAndRight = left && right
-	fmt.Printf("left && right \t = %t \n", leftAndRight)
+	// temporary variabel pada if - else
+	// variabel yang hanya bisa digunakan dalam scope seleksi kondisi saja
+	judul("temporary variabel")
+	var points = 8840.0
 
-	const leftOrRight = left || right
-	fmt.Printf("left || right \t = %t \n", leftOrRight)
+	if percent := points / 100; percent >= 100 {
+		fmt.Printf("%.1f%s perfect! \n", percent, "%")
+	} else if percent >= 70 {
+		fmt.Printf("%.1f%s good \n", percent, "%")
+	} else {
+		fmt.Printf("%.1f%s not bad \n", percent, "%")
+	}
 
-	const leftReverse = !left
-	fmt.Printf("!left \t = %t \n", leftReverse)
+	// switch case
+	// seleksi kondisi yang sifatnya pokus pada satu variabel
+	judul("switch case")
+	switch point {
+	case 8:
+		{
+			fmt.Println("just say ehlo")
+			fmt.Println("perfect")
+		}
+	case 7, 6, 5, 4:
+		fmt.Println("awesome")
+	default:
+		fmt.Println("not bad")
+	}
+
+	// switch dengan gaya if else
+	judul("switch dengan gaya if else")
+	point = 6
+	switch {
+	case point == 8:
+		fmt.Println("perfect")
+	case point < 8 && point > 3:
+		fmt.Println("awesome")
+		fallthrough // keyword untuk memaksa melanjutkan ke perintah case yang selanjutnya tanpa memperdulikan case nya benar atau salah
+	case point < 5:
+		fmt.Println("you need to learn more")
+	default:
+		{
+			fmt.Println("not bad")
+			fmt.Println("you need to learn more")
+		}
+	}
+
+	judul("seleksi kondisi bersarang")
+	point = 10
+	if point > 7 {
+		switch point {
+		case 10:
+			fmt.Println("perfect!")
+		default:
+			fmt.Println("nice!")
+		}
+	} else {
+		if point == 5 {
+			fmt.Println("Not bad")
+		} else if point == 3 {
+			fmt.Println("keep trying")
+		} else {
+			fmt.Println("you can do it")
+			if point == 0 {
+				fmt.Println("try harder!")
+			}
+		}
+	}
 }
